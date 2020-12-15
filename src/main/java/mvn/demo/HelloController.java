77c8d8016816fe15a3d463b7aedf1480a739ff17
stevem-43 <60366697+stevem-43@users.noreply.github.com>
2020-12-15 14:14:06 +0000

    Added hostname to output

--- src/main/java/mvn/demo/HelloController.java
+++ src/main/java/mvn/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Fucking Work!!!";
+		return "Docker Java Web App running on container/pod: "+ System.getenv("HOSTNAME") ;
 	}
 	
 }
